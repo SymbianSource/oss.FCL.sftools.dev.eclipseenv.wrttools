036b918ef883d38d8d694dc9c69c851602cd8880
John Kern <johnk@symbian.org>
2010-02-03 08:22:09 -0800

    provide win32 and macosx tarballs.

--- scripts/shepard.sh
+++ scripts/shepard.sh
@@ -7,11 +7,15 @@ BUILD_DIR=/home/symbian/scratch/build_dir
 DAY_OF_YEAR=`date +%j`
 HG_CMD=/usr/local/bin/hg
 
+export PATH=.:/usr/java/jdk1.6.0_18/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
+
 export WS_DIR=${BUILD_DIR}/${DAY_OF_YEAR}/workspace/wrttools
 LINUX_PROD_DIR=${BUILD_DIR}/${DAY_OF_YEAR}/linux
 LINUX_EXPORT_DIR=/home/symbian/scratch/025
 WIN32_PROD_DIR=${BUILD_DIR}/${DAY_OF_YEAR}/win32
+WIN32_EXPORT_DIR=/home/symbian/buildsFromEugene/win32.win32.x86
 MACOSX_PROD_DIR=${BUILD_DIR}/${DAY_OF_YEAR}/macosx
+MACOSX_EXPORT_DIR=/home/symbian/buildsFromEugene/macosx.cocoa.x86
 
 # display date and time 
 date
@@ -44,5 +48,31 @@ cp -p ${SCRIPTS_DIR}/supporting/* ${LINUX_PROD_DIR}/eclipse/
 
 # package it up. 
 
-tar -czvf  ${LINUX_PROD_DIR}/webruntime.linux.tar ${LINUX_PROD_DIR}/eclipse/
+cd ${LINUX_PROD_DIR}
+tar -czvf webruntime.linux.tar eclipse
+
+# Mac OSX
+cp -pr ${MACOSX_EXPORT_DIR}/eclipse ${MACOSX_PROD_DIR}
+for j in `find ${WS_DIR} -name 'org.chromium*.jar' -o -name 'org.symbian*.jar'`
+do  
+	cp -p $j ${MACOSX_PROD_DIR}/eclipse/plugins/
+done
+cp -p ${SCRIPTS_DIR}/supporting/* ${MACOSX_PROD_DIR}/eclipse/
+
+# package it up. 
+cd ${MACOSX_PROD_DIR}
+tar -czvf  webruntime.macosx.tar eclipse
+
+# Win32
+cp -pr ${WIN32_EXPORT_DIR}/eclipse ${WIN32_PROD_DIR}
+for j in `find ${WS_DIR} -name 'org.chromium*.jar' -o -name 'org.symbian*.jar'`
+do  
+	cp -p $j ${WIN32_PROD_DIR}/eclipse/plugins/
+done
+cp -p ${SCRIPTS_DIR}/supporting/* ${WIN32_PROD_DIR}/eclipse/
+
+# package it up. 
+
+cd ${WIN32_PROD_DIR}
+tar -czvf  webruntime.win32.tar eclipse
 
