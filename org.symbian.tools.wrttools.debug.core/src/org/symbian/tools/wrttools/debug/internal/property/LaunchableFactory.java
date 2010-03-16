61f47ad301bcff0b5417b64f99688b12549b9c39
Eugene Ostroukhov <eugeneo@symbian.org>
2010-03-16 11:41:07 -0700

    Some warnings were cleared

--- org.symbian.tools.wrttools.debug.core/src/org/symbian/tools/wrttools/debug/internal/property/LaunchableFactory.java
+++ org.symbian.tools.wrttools.debug.core/src/org/symbian/tools/wrttools/debug/internal/property/LaunchableFactory.java
@@ -4,12 +4,14 @@ import org.eclipse.core.runtime.IAdapterFactory;
 
 public class LaunchableFactory implements IAdapterFactory {
 
+    @SuppressWarnings("unchecked")
 	public Object getAdapter(Object adaptableObject, Class adapterType) {
 		// It is only needed to exist
 		return null;
 	}
 
-	public Class[] getAdapterList() {
+    @SuppressWarnings("unchecked")
+    public Class[] getAdapterList() {
 		// It is only needed to exist
 		return null;
 	}
